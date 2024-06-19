e3edcc02ce6d1c2a7de7eb71de347a0b72f4afa7
Andrew Stubblefield <drew@dstubblefield.com>
2024-06-19 10:05:40 -0500

    First commit Lawfirm

---
+++ core/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-162y0!73i*h$81#4^@jc%rg8opz9w2ln(16)phi&09z=sb%3jr"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
